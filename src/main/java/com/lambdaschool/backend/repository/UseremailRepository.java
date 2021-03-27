0aec48c5835f25daded126d3d267882295099671
pak11273 <pak11273@gmail.com>
2021-03-27 15:03:57 -0500

    init

---
+++ src/main/java/com/lambdaschool/backend/repository/UseremailRepository.java
@@ -0,0 +1,12 @@
+package com.lambdaschool.backend.repository;
+
+import com.lambdaschool.backend.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The CRUD Repository connecting Useremail to the rest of the application
+ */
+public interface UseremailRepository
+    extends CrudRepository<Useremail, Long>
+{
+}
